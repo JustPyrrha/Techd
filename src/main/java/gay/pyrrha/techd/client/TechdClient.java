c28a360ef48a84220d49a94818b65cfb35220f4f
Pyrrha Wills <pyrrha.social@gmail.com>
2023-02-06 14:45:07 +1000

    feat: add client initializer

---
+++ src/main/java/gay/pyrrha/techd/client/TechdClient.java
@@ -0,0 +1,19 @@
+package gay.pyrrha.techd.client;
+
+import gay.pyrrha.techd.Const;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+import static gay.pyrrha.techd.Techd.LOGGER;
+
+@ClientOnly
+public class TechdClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(ModContainer mod) {
+        var startMs = System.currentTimeMillis();
+        LOGGER.info("[{}|Client] Initializing...", Const.MOD_NAME);
+
+        LOGGER.info("[{}|Client] Initialized. (Took {}ms)", Const.MOD_NAME, System.currentTimeMillis() - startMs);
+    }
+}
